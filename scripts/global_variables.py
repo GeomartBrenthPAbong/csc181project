ba579643a45d838f62f0618ddfdeebef7026c046
Geomart Brenth Abong <n2txheen@gmail.com>
2014-09-11 22:31:51 +0800

    Placed metas and title to the header and added a default location for the title. Sample codes for general_additions and global_variables wre removed

--- scripts/global_variables.py
+++ scripts/global_variables.py
@@ -12,3 +12,11 @@ g_session = None
 
 g_notification_title = None
 g_notification_msg = None
+
+## Replace this with the form object soon
+g_login_form = '<form class="form-signin" role="form">' +\
+				'<input type="email" class="form-control" placeholder="Email address" required autofocus/><br>'+\
+				'<input type="password" class="form-control" placeholder="Password" required/><br>' +\
+				'<input type="checkbox" id="remember-me" value="remember-me"/> <label for="remember-me">Remember me</label><br>' +\
+				'<input type="submit" class="btn btn-lg btn-primary btn-block" value="Sign in">' +\
+				'</form>'
--- scripts/page_templates/home_template.py
+++
@@ -1,6 +0,0 @@
-import scripts.global_variables
-
-def processTemplate():
-	scripts.global_variables.g_str_body = '<div><p>home template ' + scripts.global_variables.g_str_content + '</p></div>'
-
-scripts.global_variables.g_process_template = processTemplate
