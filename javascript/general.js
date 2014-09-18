69966bd3a4aed849bb8094161588f92e6886d89f
Geomart Brenth Abong <n2txheen@gmail.com>
2014-09-19 07:48:25 +0800

    Implemented the ajaxify function. This must be used whenever ajax related things will be implemented. Default success function and error function were also implemented.

--- javascript/general.js
+++ javascript/general.js
@@ -1,13 +1,30 @@
-jQuery(document).ready( function($) {
-    var color = true;
-    setInterval( justASimpleEffects, 1000 );
+function success_func(res) {
+    alert(res.msg);
+}
 
-    function justASimpleEffects() {
-        if(color)
-            $('#content').css( 'background-color', '#C2BFBF');
-        else
-            $('#content').css( 'background-color', '#FFFFFF');
-        color = !color;
-    }
-});
+function error_func(){
+    alert('Something went wrong!');
+}
 
+function ajaxify(p_data, p_success_func, p_error_func) {
+    if(!p_success_func)
+        p_success_func = success_func;
+    if(!p_error_func)
+        p_error_func = error_func;
+
+    $.ajax({
+            url: 'http://localhost/spam/scripts/ajax.py',
+            dataType: 'json',
+            traditional: true,
+            async: false,
+            type: 'POST',
+            global: true,
+            data: p_data,
+            success: function( response ) {
+                p_success_func(response);
+            },
+            error: function() {
+                p_error_func();
+            }
+        });
+}
