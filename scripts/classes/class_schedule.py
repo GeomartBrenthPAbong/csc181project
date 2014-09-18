81bc1e5c0878c4cf709f9af74057b74cbf5a891d
Geomart Brenth Abong <n2txheen@gmail.com>
2014-09-18 13:42:23 +0800

    Modified the implementaion of schedule class

--- scripts/classes/class_schedule.py
+++ scripts/classes/class_schedule.py
@@ -1,14 +1,14 @@
-import class_dosql as sql
-
 class Schedule(object):
-
-	__m_sql = sql.doSql()
-
 	def __init__ (self):
 		self.__m_id = None
 		self.__m_from_time = None
 		self.__m_to_time = None
-		self.__m_query = None
+
+	##==============================
+	## Getters and Setters
+
+	def setScheduleID(self, p_id):
+		self.__m_id = p_id
 
 	def setFromTime(self, p_from_time):
 		self.__m_from_time = p_from_time
@@ -16,33 +16,99 @@ class Schedule(object):
 	def setToTime(self, p_to_time):
 		self.__m_to_time = p_to_time
 
+	def getScheduleID(self):
+		return self.__m_id
+
+	def getFromTime(self):
+		return self.__m_from_time
+
+	def getToTime(self):
+		return self.__m_to_time
+
+	##==============================
+	## Other functions
+
+	### Creates a Schedule object using time
+	###
+	### @param string p_from_time
+	### @param string p_to_time
+	### @return Schedule
+	### @throws ENotEnoughDetails
+	### @throws EInvalidFormat
 	@staticmethod
-	def createObject((p_from_time, p_to_time, p_sched_day)):
+	def createObject((p_from_time, p_to_time)):
+		import scripts.exceptions.e_notenoughdetails as e_notenoughdetails
+
+		if p_from_time is None or \
+			p_to_time is None:
+				raise e_notenoughdetails.ENotEnoughDetails('From time and to time are required.')
+
+		import scripts.functions as functions
+		import scripts.exceptions.e_invalidformat as e_invalidformat
+
+		if not functions.is_time_format(p_from_time):
+			raise e_invalidformat.EInvalidFormat('From time is invalid.')
+
+		if not functions.is_time_format(p_to_time):
+			raise e_invalidformat.EInvalidFormat('To time is invalid.')
+
 		schedule = Schedule()
 		schedule.setFromTime(p_from_time)
 		schedule.setToTime(p_to_time)
 		return schedule
 
-	def dbStore(self):
-		self.__m_query = "createSchedule( "
-		self.__m_query += self.__m_from_time + ","
-		self.__m_query += self.__m_to_time + ")"
-		[self.__m_id] = __m_sql.execqry(self.__m_query, True)
-		#provided that createScheule in dbsql should return us
-		#the id of this schedule 
-
+	### Creates a Schedule object using an id
+	###
+	### @param int p_schedule_id
+	### @return Schedule
+	### @throws ENotRegistered
 	@staticmethod
-	def editSchedule((p_sched_id, p_new_from_time, p_new_to_time)):
-		self.__m_query = "checkSchedExistence( " + str(p_sched_id) + ")"
-		[self.__m_id] = __m_sql.execqry(self.__m_query, False)
-		
-		#if p_sched_id is existing
+	def dbExtract(p_schedule_id):
+		import scripts.global_variables as g
+		import scripts.exceptions.e_notregistered as e_notregistered
 
+		try:
+			[(
+				_,
+				from_time,
+				to_time
+			)] = g.g_sql.execqry('extractSchedInfoFromSchedID(' + p_schedule_id + ')')
 
+			schedule = Schedule()
+			schedule.setScheduleID(p_schedule_id)
+			schedule.setFromTime(from_time)
+			schedule.setToTime(to_time)
 
+			return schedule
+		except:
+			raise e_notregistered.ENotRegistered('Schedule does not exists.')
 
+	### Stores the schedule to the schedule table
+	###
+	### @throws ENotEnoughDetails
+	### @throws EInvalidFormat
+	### @throws EAlreadyExists
+	def dbStore(self):
+		import scripts.exceptions.e_notenoughdetails as e_notenoughdetails
 
+		if self.__m_id is None or \
+			self.__m_from_time is None or \
+			self.__m_to_time is None:
+				raise e_notenoughdetails.ENotEnoughDetails('All schedule details are required.')
 
+		import scripts.functions as functions
+		import scripts.exceptions.e_invalidformat as e_invalidformat
 
-		
-		 
+		if not functions.is_time_format(self.__m_from_time):
+			raise e_invalidformat.EInvalidFormat('From time is invalid.')
+
+		if not functions.is_time_format(self.__m_to_time):
+			raise e_invalidformat.EInvalidFormat('To time is invalid.')
+
+		import scripts.global_variables as g
+		import scripts.exceptions.e_alreadyexists as e_alreadyexists
+
+		if functions.schedule_time_exists(self.__m_from_time, self.__m_to_time):
+			raise e_alreadyexists.EAlreadyExists('Schedule already exists.')
+
+		[self.__m_id] = g.g_sql.execqry('createSchedule(' + self.__m_from_time + ', '+ self.__m_to_time + ')', True)
