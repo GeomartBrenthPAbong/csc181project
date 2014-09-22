d405f09eead4f153e77be76ce9bd3fb9de3a0866
janssensnow <tjanssenjames@yahoo.com>
2014-09-22 21:38:25 +0800

    changed the background of all layouts,created a create appointment page for student profile with a beta search bar that has a drop down function using jquery

--- scripts/page_templates/black_template.py
+++ scripts/page_templates/black_template.py
@@ -28,14 +28,7 @@ def generate_page():
 							'<h2 class="title">' + g.g_content.getTitle() + '</h2>' +\
 							g.g_locations.printContentsAtLocation('between_title_content') +\
 							'<div class="main-content">' +\
-								'<div class="left-content left">' +\
-									g.g_content.getContent() +\
-									g.g_locations.printContentsAtLocation('left_content') +\
-								'</div>' +\
-								'<div class="right-content left">' +\
-									g.g_locations.printContentsAtLocation('right_content') +\
-								'</div>' +\
-								'<div class="clearfix"></div>' +\
+								g.g_content.getContent() +\
 							'</div><!-- end main-content -->' +\
 							g.g_locations.printContentsAtLocation('after_content') +\
 						'</div><!-- container -->' +\
--- scripts/pages/studhome.py
+++ scripts/pages/studhome.py
@@ -1,62 +1,41 @@
 import scripts.global_variables as g
 import scripts.classes.class_printable as p
-import scripts.classes.class_student as s
-import scripts.functions as f
 
 def get_title():
 	return 'Student Profile'
 	
 def get_content():
-	return ""
+	html =	"""
+			<div id = "picture">
+				<img src="../spam/picture/user/student/2009-0731.png" alt="No picture found." style="width:290px;height:290px">
+			</div>
+			"""
+	
+	return html
 	
 	
 def get_page_template():
 	return 'black_template'
 	
 def page_additions():
+	g.g_header.getStyleAdder().add('studhome')
+	#stud_name = g.g_user.getFirstName() + " " + g.g_user.getLastName()
+	stud_name = "Christopher Pacillos"
 	
-	#if not f.user_logged_in():
-		#pass
-		#use a redirect function
-		
-	g.g_user = s.Student()
-	g.g_user.setAddress("Tibanga")
-	g.g_user.setID("2009-0731")
-	g.g_user.setFirstName("Christopher Clint")
-	g.g_user.setLastName("Pacillos")
-	g.g_user.setEmailAddress("clint.pacillos@gmail.com")
-	g.g_user.setPhoneNumber("09122241144")
-	g.g_user.setCourse("BSEC")
-	
-	g.g_header.getStyleAdder().add('profile')
-	
-	stud_name = g.g_user.getFirstName() + " " + g.g_user.getLastName()
-	stud_id = g.g_user.getID()
-	stud_email = g.g_user.getEmailAddress()
-	stud_phone = g.g_user.getPhoneNumber()
-	stud_address = g.g_user.getAddress()
-	stud_course = g.g_user.getCourse()
-	
-	
-	details_html =	'<div class="left" id="details">'
-	details_html +=	"<h1>" + stud_name + "</h1>"
-	details_html +=	"<p>" + stud_course + "</p>"
-	details_html +=	"<p>" + stud_email + "</p>"
-	details_html +=	"<p>" + stud_phone + "</p>"
-	details_html +=	"<p>" + stud_address + "</p>"
-	details_html +=	"<p>" + stud_id + "</p>"
-	details_html += "</div>"
-	details_html += '<div class="clearfix"></div>'
-	
-	user_pic_html =	"""
-					<div class="left" id="picture">
-						<img src="../spam/picture/user/student/2009-0731.png" alt="No picture found." style="width:290px;height:290px">
+	details_html =	"""					
+					<div id="details">
+						<h1>Christopher Clint Pacillos</h1>
+					<p>
+						Bachelor of Science in Compputer Engineering
+					</p>
+					<p>
+						<br>Some other details here...
+					</p>
 					</div>
 					"""
 	g.g_locations.addToLocation('head_title', p.Printable("Home-" + stud_name))
-	g.g_locations.addToLocation('left_content', p.Printable(user_pic_html + details_html))
-	g.g_locations.addToLocation('right_content', p.Printable("Search here."))
-	
+	g.g_locations.addToLocation('after_content', p.Printable(details_html))
+	g.g_locations.addToLocation('footer', p.Printable("These are some footer content."))
 	
 	content_list = g.g_locations.getContentsAtLocation('main_nav')
 	if len(content_list) is 1:
