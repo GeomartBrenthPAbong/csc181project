8239a3ac38108db3aeed5bc1c8c0e267b6a953c5
Geomart Brenth Abong <n2txheen@gmail.com>
2014-09-28 19:27:07 +0800

    Commited some changes

--- ajax.py
+++ ajax.py
@@ -1,8 +1,10 @@
 def index(req):
 	import os,sys
 	import scripts.third_party_modules.simplejson.simplejson as json
+	import global_variables as g
 
-	sys.path.append(os.path.dirname(__file__))
+	g.g_main_path = os.path.dirname(__file__)
+	sys.path.append(g.g_main_path)
 
 	import functions
 	import ajax_functions
@@ -13,7 +15,7 @@ def index(req):
 		if function_name is None:
 			raise Exception
 		try:
-			if functions.is_user_logged_in():
+			if functions.user_logged_in():
 				return getattr(ajax_functions, 'spam_in_' + function_name)(req)
 			raise AttributeError
 		except AttributeError:
