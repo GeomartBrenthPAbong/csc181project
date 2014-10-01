cd8038fc712a7642fb328d035ffe132608cfd1fa
Geomart Brenth Abong <n2txheen@gmail.com>
2014-10-01 14:52:40 +0800

    Added an initialization line for user to accomodate login feature

--- index.py
+++ index.py
@@ -12,11 +12,13 @@ def index(req, page = 'login_page'):
 	import scripts.functions
 	import scripts.general_additions
 
+	scripts.global_variables.g_req = req
 	scripts.global_variables.g_root_path = 'http://localhost/spam'
 	scripts.global_variables.g_main_path = home_path
 
-	scripts.functions.page_validation( page )
+	scripts.functions.page_validation(page)
 
+	scripts.global_variables.g_user = scripts.functions.create_user()
 	scripts.functions.pre_processing()
 	scripts.general_additions.include_in_pages()
 	scripts.functions.process_page()
