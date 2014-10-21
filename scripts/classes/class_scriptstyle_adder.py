2fb80e68c99796d15c6c6bce33fbac48a69b81e0
janssensnow <tjanssenjames@yahoo.com>
2014-10-21 22:30:03 +0800

    Added getList method.

--- scripts/classes/class_scriptstyle_adder.py
+++ scripts/classes/class_scriptstyle_adder.py
@@ -2,6 +2,22 @@ class ScriptStyleAdder(object):
 
 	def __init__(self):
 		self._m_path_list = []
+		self._m_type = None
+		self._m_folder = None
 
 	def add(self, p_path):
 		self._m_path_list.append(p_path)
+
+	def getList(self, p_exclude = []):
+		if not self._m_path_list:
+			return []
+
+		import scripts.global_variables as g
+
+		fullpath_list = []
+
+		for path in self._m_path_list:
+			if p_exclude and path in p_exclude:
+				continue
+			fullpath_list.append(g.g_root_path + '/' + self._m_folder + '/' + path + '.' + self._m_type)
+		return fullpath_list
